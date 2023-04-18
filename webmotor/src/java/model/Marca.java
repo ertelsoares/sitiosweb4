f199530cb44eaaf115404d71194279048c1ba1cf
Lucas Ertel <54690363+ertelsoares@users.noreply.github.com>
2023-04-18 19:33:07 -0300

    Pacote dao

--- webmotor/src/java/model/Marca.java
+++ webmotor/src/java/model/Marca.java
@@ -4,10 +4,39 @@
  */
 package model;
 
-/**
- *
- * @author aluno
- */
+
 public class Marca {
+    private int id;
+    private String nome;
+    private Marca marca;
+
+    public Marca() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+    
+    
     
 }
--- webmotor/src/java/model/Veiculo.java
+++ webmotor/src/java/model/Veiculo.java
@@ -4,10 +4,74 @@
  */
 package model;
 
-/**
- *
- * @author aluno
- */
+
 public class Veiculo {
+    private String placa;
+    private Modelo modelo;
+    private double valor;
+    private int ano,km;
+    private String cor;
+
+    
+
+    public Veiculo(String placa, Modelo modelo, double valor, int ano, int km, String cor) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.valor = valor;
+        this.ano = ano;
+        this.km = km;
+        this.cor = cor;
+    }
+    public Veiculo() {
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(Modelo modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getKm() {
+        return km;
+    }
+
+    public void setKm(int km) {
+        this.km = km;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    
     
 }
